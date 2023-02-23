11e9b53e8d5d7d92a208efed694eb2f3eb68d7c6
Takeaki0817 <s.takeaki0817@gmail.com>
2023-02-23 16:07:02 +0900

    🎨 コードを整形

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import type { NextPage } from 'next';
-import { Box, Heading } from '@chakra-ui/react';
+import type { NextPage } from 'next'
+import { Box, Heading } from '@chakra-ui/react'
 
 const Page: NextPage = () => {
-	return (
-		<Box>
-			<Heading>Chakra UI</Heading>
-		</Box>
-	);
-};
+  return (
+    <Box>
+      <Heading>Chakra UI</Heading>
+    </Box>
+  )
+}
 
-export default Page;
+export default Page
